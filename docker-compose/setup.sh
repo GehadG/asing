6fa1ea897d4700ac3cde567efacebf003c265e12
Gehad <gehad4195@gmail.com>
2021-07-11 21:08:30 +0200

    Finalized

--- docker-compose/setup.sh
+++ docker-compose/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+docker-compose exec app composer config --global github-oauth.github.com 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
 docker-compose exec app composer install --no-interaction --prefer-dist --optimize-autoloader
 
 docker-compose exec app php artisan storage:link || true
